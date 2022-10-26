858f9c8f2d73e11534335a62614443516b7bbdd2
Camilla Sundbom <camilla.sundbom@outlook.com>
2022-10-26 17:46:10 +0200

    added guard for guesses over 100

--- Workshop 6 - JavaScript CodeJam/script.js
+++ Workshop 6 - JavaScript CodeJam/script.js
@@ -10,6 +10,10 @@ function guessNumber() {
   const guess = document.getElementById("guess").value;
 
   switch (true) {
+    case guess > 100:
+      message3.innerText = "Please pick a number between 0 and 100!";
+      break;
+
     case guess < randomNumber:
       message3.innerText = "Higher!";
       no_of_guesses++;
