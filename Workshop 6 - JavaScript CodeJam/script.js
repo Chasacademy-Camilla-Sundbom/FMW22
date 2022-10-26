8bb4b75a2fd30a69ffda111b769c75e16ef632f0
Camilla Sundbom <camilla.sundbom@outlook.com>
2022-10-26 17:37:53 +0200

    created new function for number guessing game

--- Workshop 6 - JavaScript CodeJam/script.js
+++ Workshop 6 - JavaScript CodeJam/script.js
@@ -2,14 +2,38 @@ const message3 = document.getElementById("message3");
 const message2 = document.getElementById("message2");
 const message1 = document.getElementById("message1");
 
-const answer = Math.floor(Math.random() * 100) + 1;
-const no_of_guesses = 0;
-const guessed_nums = [];
+const randomNumber = Math.floor(Math.random() * 101);
+let no_of_guesses = 0;
+const guessedNumbers = [];
 
-function play() {
-  const user_guess = document.getElementById("guess").value; //flytta??
-  if (user_guess < 0 || user_guess > 100) {
-    alert("Please enter a number between 0 and 100.");
-  } else {
+function guessNumber() {
+  const guess = document.getElementById("guess").value;
+
+  switch (true) {
+    case guess < randomNumber:
+      message3.innerText = "Higher!";
+      no_of_guesses++;
+      guessedNumbers.push(guess);
+      message1.innerText = `No. Of Guesses: ${no_of_guesses}`;
+      break;
+
+    case guess > randomNumber:
+      message3.innerText = "Lower!";
+      no_of_guesses++;
+      guessedNumbers.push(guess);
+      message1.innerText = `No. Of Guesses: ${no_of_guesses}`;
+      break;
+
+    case guess == randomNumber:
+      message3.innerText = "Correct!";
+      no_of_guesses++;
+      guessedNumbers.push(guess);
+      message1.innerText = `No. Of Guesses: ${no_of_guesses}`;
+      break;
+
+    default:
+      break;
   }
+
+  message2.innerText = guessedNumbers.join(", ");
 }
