4ced30f5ef1824c8819ac4f891b372a553e69d87
Camilla Sundbom <camilla.sundbom@outlook.com>
2022-10-31 13:03:33 +0100

    Did some exercises in coding, among exemple JS

---
+++ JavaScript - Övningar/script.js
@@ -0,0 +1,43 @@
+/*function displayDate() {
+  document.getElementById("demo").innerHTML = Date();
+}*/
+
+/*const person = {
+  firstName: "Camilla",
+  SureName: "Sundbom",
+  age: 38,
+  eyeColor: "blue",
+};
+document.getElementById("demo").innerHTML =
+  person.firstName + " is " + person.age + " yeras old.";*/
+
+/*let x = myFunction(4, 3);
+document.getElementById("demo").innerHTML = x;
+function myFunction(a, b) {
+  return a * b;
+}*/
+
+/*function myfunction(p1, p2) {
+  return p1 * p2;
+}
+document.getElementById("demo").innerHTML = myfunction(4, 3);*/
+
+/*let carName = "Volvo";
+document.getElementById("demo").innerHTML = carName;*/
+
+//denna deklareras med const nyckelord och är konstanta, kan ej ändras
+/*const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;// nyckelord//
+document.getElementById("demo").innerHTML = "The total is: " + total;*/
+
+//denna deklareas med en let nyckelord och kan ändras//
+/*let x = 5;
+let y = 6;
+let z = x + y;//nyckelord//
+
+document.getElementById("demo").innerHTML = "The value of z is: " + z;*/
+
+/*function myfunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}*/
