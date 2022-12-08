b5fa84d667b95a523c2e55bffa9aee89c5acc919
Camilla Sundbom <Camilla.Sundbom@outlook.com>
2022-12-08 10:29:08 +0100

    did some codealong

---
+++ Lektion_Datum_Tid/script.js
@@ -0,0 +1,34 @@
+/*const now2 = new Date();
+console.log(now2);
+//Wed Dec 07 2022 09:40:33 GMT+0100 (Central European Standard Time)
+
+// Tidstämplar
+console.log(now.getTime());
+// 1670402837372 sedan 1970 i millisekunder epoktime
+
+const epochTime2 = new Date(0); //den tiden får när sekunder började räknas
+console.log(epochTime2);
+//Thu Jan 01 1970 01:00:00 GMT+0100 (Central European Standard Time)*/
+
+//skapa tid med Date.
+/*const now = new Date(); //skapr tiden just nu.
+const epochTime = new Date(1000); // använder tidsstämpel
+const date = new Date("2000-01-01"); // "yyyy-mm-dd" "dd-mm-yyyy"
+console.log(date);
+//Sat Jan 01 2000 01:00:00 GMT+0100 (Central European Standard Time)
+const dateWithNumber = new Date(1998, 11, 24, 12, 00, 00); //yea, month, day, hours, minutes, second, millisecond
+console.log(dateWithNumber);
+//Mon Mar 02 1998 10:03:03 GMT+0100 (Central European Standard Time)
+
+//Uppgift testa 4 sätt att skapa datum.
+
+const today = new Date();
+console.log(today.getDay());
+//Uppgift hitta funktioner i date som hämater en del av tiden eller datum.
+const yesterday = new Date("2022-12-06");
+console.log(yesterday.getTime());
+console.log(today.getTime());
+
+console.log(today.getTime() > yesterday.getTime());*/
+
+//https://jsonplaceholder.typicode.com/todos
